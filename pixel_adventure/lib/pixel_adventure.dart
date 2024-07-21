a303dfb4f44db6a4c47202892e435e83b5309aac
z1nl0x <paulo.kreft@gmail.com>
2024-07-21 16:29:51 -0300

    Project updates

---
+++ pixel_adventure/lib/pixel_adventure.dart
@@ -0,0 +1,28 @@
+import 'dart:async';
+import 'dart:ui';
+import 'package:flame/components.dart';
+import 'package:flame/game.dart';
+import 'package:pixel_adventure/levels/level.dart';
+
+class PixelAdventure extends FlameGame {
+
+  @override
+  Color backgroundColor() => const Color(0xFF211F30);
+
+  late final CameraComponent cam;
+
+  final world = Level(levelName: 'Level-02');
+
+  @override
+  FutureOr<void> onLoad() async {
+    // Load all images into cache
+    await images.loadAllImages();
+    
+    cam = CameraComponent.withFixedResolution(world: world, width: 640, height: 360);
+    cam.viewfinder.anchor = Anchor.topLeft;
+
+    addAll([cam, world]);
+
+    return super.onLoad();
+  }
+}
